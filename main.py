523fe294a192404c699d1e0c1178e7ff395b5886
Van Lam Nguyen <vanlam1412@gmail.com>
2023-09-15 15:37:42 +0700

    update

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://api-cve-scan-project.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
